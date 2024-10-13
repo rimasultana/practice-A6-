851a9d756146bcb9b5b1fdf638773ea148ad9c8e
rimasultana <rimasultana20225@gmail.com>
2024-10-13 22:27:44 +0600

    button done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const loadCategoryData = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/peddy/categories"
+  );
+  const data = await res.json();
+  displayCategoryData(data.categories);
+};
+
+const displayCategoryData = (categories) => {
+    console.log(categories);
+    const categoryContainer = document.getElementById("category");
+  
+    categories.forEach((element) => {
+      const div = document.createElement("div");
+      div.innerHTML = `
+        <div class='btn border-2 border-amber-400 flex  items-center justify-center  text-center '>
+        <img class='w-8 object-cover' src='${element.category_icon}'/>
+          <p>${element.category}</p>
+        </div>
+      `;
+      categoryContainer.appendChild(div); 
+    });
+  };
+
+loadCategoryData();
